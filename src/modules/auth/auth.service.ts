36f1895107d67bfbef7f906876b6aa863e9e1f78
umkolove <umarbedelbaev@gmail.com>
2024-02-19 14:05:46 +0600

    add impomanted signup

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+    constructor(private readonly userService: UsersService) {}
+    async signup(userData) {
+        return await this.userService.create(userData);
+
+    }
+}
+  
+
+
